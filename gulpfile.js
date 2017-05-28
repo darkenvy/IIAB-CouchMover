2f4055ae68fc8e50e458d68f85c4893a12f8560f
darkenvy <darkenvy6@gmail.com>
2017-05-28 15:36:46 -0700

    merged Tim's menu with mine. Changed link paths and cleaned up

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@
 let fs = require('fs');
 let gulp = require('gulp');
 let GulpSSH = require('gulp-ssh');
+let beep = require('beepbeep')
+
 
 let publicFolder = './public/';
-var config = {
+let config = {
   host: 'schoolserver.local',
   port: 22,
   username: 'pi',
   password: 'raspberry'
 }
-var gulpSSH = new GulpSSH({
+let gulpSSH = new GulpSSH({
   ignoreErrors: false,
   sshConfig: config
 });
@@ -18,8 +20,8 @@ var gulpSSH = new GulpSSH({
 
 gulp.task('watch', function() {
   gulp.watch(publicFolder + 'img/**/*', ['all']);
-  gulp.watch(publicFolder + 'css/*.css', ['copy-css']);
-  gulp.watch(publicFolder + 'js/*.css', ['copy-js']);
+  gulp.watch(publicFolder + 'css/**/*.css', ['copy-css']);
+  gulp.watch(publicFolder + 'js/**/*.js', ['copy-js']);
   gulp.watch(publicFolder + 'index.html', ['copy-index']);
   gulp.watch('index.js', ['copy-node-index']);
 });
@@ -28,23 +30,27 @@ gulp.task('copy-all', function () {
   return gulp
     .src([publicFolder + '**/*', '!**/node_modules/**'])
     .pipe(gulpSSH.dest('/library/www/html/home/'))
+    .on('end', ()=> {beep([250,250])})
 })
 
 gulp.task('copy-css', function () {
   return gulp
     .src([publicFolder + 'css/**/*'])
     .pipe(gulpSSH.dest('/library/www/html/home/css/'))
+    .on('end', ()=> {beep([250,250])})
 })
 
 gulp.task('copy-js', function () {
   return gulp
     .src([publicFolder + 'js/**/*'])
     .pipe(gulpSSH.dest('/library/www/html/home/js/'))
+    .on('end', ()=> {beep([250,250])})
 })
 
 gulp.task('copy-index', function () {
   return gulp.src(publicFolder + 'index.html')
     .pipe(gulpSSH.sftp('write', '/library/www/html/home/index.html'))
+    .on('end', ()=> {beep([250,250])})
 })
 
 gulp.task('copy-node-index', function () {
@@ -52,10 +58,11 @@ gulp.task('copy-node-index', function () {
     .pipe(gulpSSH.sftp('write', '/library/www/html/home/node-couchmover.js'))
     .pipe(gulpSSH.exec(['node /library/www/html/home/node-couchmover.js'], {filePath: 'commands.log'})
     .on('ssh2Data', (chunk)=>{
-      console.log(chunk.toString())
+      console.log(chunk.toString());
+      beep([250,250])
     }))
     
 })
 
-// gulp.task('all', ['copy-all']);
+gulp.task('all', ['copy-all']);
 gulp.task('default', ['watch']);
